43a80667d2167d01d60eb30349de3905a4273503
filipicaldas <50049560+filipicaldas@users.noreply.github.com>
2019-05-09 12:24:23 -0300

    Add files via upload

---
+++ src/service/CampeonatoService.java
@@ -0,0 +1,93 @@
+package service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.simpleframework.http.Query;
+import org.simpleframework.http.Request;
+
+import dao.CampeonatoDAO;
+import model.Campeonato;
+
+
+public class CampeonatoService {
+	private CampeonatoDAO campeonatoDAO = new CampeonatoDAO();
+	public String consultarCampeonato(Integer id, Request request) {
+		try {
+			Campeonato campeonato = campeonatoDAO.get(id);
+			return campeonato.toJson().toString();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Erro ao consultar Campeonato";
+		}
+	}
+	public String listarCampeonato(Request request) {
+		try {
+			return listaCampeonatoJSON().toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Erro ao consultar Campeonatos";
+		}
+	}
+	public String adicionarCampeonato(Request request) {
+		Query query = request.getQuery();
+
+		try {
+			Campeonato campeonato = new Campeonato();
+			campeonato.setNome(query.get("nome"));
+			campeonato.setJogos(query.getInteger("jogos"));
+			campeonatoDAO.add(campeonato);
+
+			return listaCampeonatoJSON().toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Erro ao adicionar Campeonato";
+		}
+	}
+	public String atualizarCampeonato(Request request) {
+		Query query = request.getQuery();
+
+		try {
+			Campeonato campeonato = new Campeonato();
+			campeonato.setNome(query.get("nome"));
+			campeonato.setJogos(query.getInteger("jogos"));
+			campeonatoDAO.update(campeonato);
+
+			return listaCampeonatoJSON().toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Erro ao atualizar o campeonato";
+		}
+	}
+	public String removerCampeonato(Request request) {
+		Query query = request.getQuery();
+		
+		try {
+			Campeonato campeonato = new Campeonato();
+			campeonato.setId(query.getInteger("id"));
+			
+			campeonatoDAO.delete(campeonato);
+			
+			return listaCampeonatoJSON().toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Erro ao excluir Jogador.";
+		}
+	}
+
+	private JSONObject listaCampeonatoJSON() throws NumberFormatException, IOException {
+		List<Campeonato> listaCampeonatos = campeonatoDAO.getAll();
+
+		JSONArray array = new JSONArray();
+		for (Campeonato j : listaCampeonatos) {
+			array.put(j.toJson());
+		}
+		JSONObject obj = new JSONObject();
+		obj.put("listaCampeonatos", new JSONArray(listaCampeonatos));
+
+		return obj;
+	}
+}
